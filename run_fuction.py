73e45ba41f9ee4c891578bd2ffbcd9ef8f33e686
hilge <johanthygesen@hotmail.com>
2023-01-19 14:39:01 +0000

    better naming of cencus file

--- run_fuction.py
+++ run_fuction.py
@@ -9,7 +9,7 @@ import calc_adjust_rate
 # Load data
 participants = pd.read_csv("data/participants.txt", sep='\t')
 diseases = pd.read_csv("data/diseases.txt", sep='\t')
-census = pd.read_csv("data/census.txt", sep='\t')
+census = pd.read_csv("data/english_census_2021.txt", sep='\t')
 
 # Calculate the adjusted rates
 calc_adjust_rate.age_sex(participants, diseases, census, pd, np, popsize = 10000)
